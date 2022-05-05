a78a32c2aeaae1fb8f77bd22662844808e3c3cd3
rupertluo <1225457706@qq.com>
2022-05-05 06:55:17 +0000

    complete task2 dataset and dataloader

--- mid-homework/utils/data_utils.py
+++ mid-homework/utils/data_utils.py
@@ -1,1 +1,31 @@
-#
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+def collate_fn(batch):
+    """
+    To handle the data loading as different images may have different number 
+    of objects and to handle varying size tensors as well.
+    """
+    return tuple(zip(*batch))
+
+# define the training tranforms
+def get_train_transform():
+    return A.Compose([
+        A.Flip(0.5),
+        A.RandomRotate90(0.5),
+        A.MotionBlur(p=0.2),
+        A.MedianBlur(blur_limit=3, p=0.1),
+        A.Blur(blur_limit=3, p=0.1),
+        ToTensorV2(p=1.0),
+    ], bbox_params={
+        'format': 'pascal_voc',
+        'label_fields': ['labels']
+    })
+
+# define the validation transforms
+def get_valid_transform():
+    return A.Compose([
+        ToTensorV2(p=1.0),
+    ], bbox_params={
+        'format': 'pascal_voc', 
+        'label_fields': ['labels']
+    })
